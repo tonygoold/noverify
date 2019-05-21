f6834ad9ed5730ec953f541010cfda6d1f0abf4a
R. Tony Goold <tony@goold.net>
2019-05-21 10:19:41 -0400

    Fix handling of calling new with a variable
    This fixes a bug where "new $x" would not mark $x as used.

--- src/linter/block.go
+++ src/linter/block.go
@@ -1202,7 +1202,8 @@ func (b *BlockWalker) handleNew(e *expr.New) bool {
 
 	className, ok := solver.GetClassName(b.r.st, e.Class)
 	if !ok {
-		// perhaps something like 'new $class', cannot check this.
+		// perhaps something like 'new $class'
+		e.Class.Walk(b)
 		return false
 	}
 
